1a8da24adc84356f49fb612aa535358085a02007
Kamil Kowalski <kowalski.tychy@gmail.com>
2018-07-31 11:52:31 +0200

    Add typespecs to Helpers module

--- lib/plug_devise_session/helpers.ex
+++ lib/plug_devise_session/helpers.ex
@@ -3,11 +3,16 @@ defmodule PlugDeviseSession.Helpers do
   Helpers that assist in working with session fetched via `PlugDeviseSession` plug.
   """
 
+  @type id :: integer
+  @type salt :: String.t()
+  @type scope :: atom | String.t()
+
   alias Plug.Conn
 
   @doc """
   Removes user auth data, optionally from a specified scope.
   """
+  @spec delete_user_auth_data(Plug.Conn.t(), scope) :: Plug.Conn.t()
   def delete_user_auth_data(conn, scope \\ :user) do
     Conn.delete_session(conn, "warden.user.#{scope}.key")
   end
@@ -16,6 +21,7 @@ defmodule PlugDeviseSession.Helpers do
   Returns currently logged-in user's identifier, optionally in specified scope.
   """
   @deprecated "Use get_user_auth_data/2 instead"
+  @spec get_user_id(Plug.Conn.t(), scope) :: Plug.Conn.t()
   def get_user_id(conn, scope \\ :user) do
     case Conn.get_session(conn, "warden.user.#{scope}.key") do
       [[id], _] when is_integer(id) ->
@@ -32,6 +38,7 @@ defmodule PlugDeviseSession.Helpers do
   @doc """
   Returns currently logged-in user's id and auth salt, optionally in specified scope.
   """
+  @spec get_user_auth_data(Plug.Conn.t(), scope) :: Plug.Conn.t()
   def get_user_auth_data(conn, scope \\ :user) do
     case Conn.get_session(conn, "warden.user.#{scope}.key") do
       [[id], auth_salt] when is_integer(id) ->
@@ -48,6 +55,7 @@ defmodule PlugDeviseSession.Helpers do
   @doc """
   Puts user's id and auth salt, optionally in specified scope.
   """
+  @spec put_user_auth_data(Plug.Conn.t(), id, salt, scope) :: Plug.Conn.t()
   def put_user_auth_data(conn, id, auth_salt, scope \\ :user) do
     Conn.put_session(conn, "warden.user.#{scope}.key", [[id], auth_salt])
   end
