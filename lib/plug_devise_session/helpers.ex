57c918f28a71a0ef3662a9725f8b4485770a9fc4
Artur Sulej <artur@shedul.com>
2019-04-08 14:24:08 +0200

    Helper for putting random session_id in the session

--- lib/plug_devise_session/helpers.ex
+++ lib/plug_devise_session/helpers.ex
@@ -59,4 +59,19 @@ defmodule PlugDeviseSession.Helpers do
   def put_user_auth_data(conn, id, auth_salt, scope \\ :user) do
     Conn.put_session(conn, "warden.user.#{scope}.key", [[id], auth_salt])
   end
+
+  @doc """
+  Puts session id.
+  """
+  @spec put_session_id(Plug.Conn.t(), integer) :: Plug.Conn.t()
+  def put_session_id(conn, length) do
+    Conn.put_session(conn, "session_id", random_string(length))
+  end
+
+  defp random_string(length) do
+    length
+    |> :crypto.strong_rand_bytes()
+    |> Base.url_encode64()
+    |> binary_part(0, length)
+  end
 end
