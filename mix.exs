8d4e09d84c1b265710621e9de6584f0779915f72
Kamil Kowalski <kamil@kamilkowalski.pl>
2022-04-11 23:40:53 +0200

    Upgrade plug_rails_cookie_session_store to 2.0 (#13)

--- mix.exs
+++ mix.exs
@@ -4,20 +4,16 @@ defmodule PlugDeviseSession.Mixfile do
   def project do
     [
       app: :plug_devise_session,
-      version: "0.9.0",
-      elixir: "~> 1.9.1",
+      version: "1.0.0",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
       aliases: aliases(),
-      test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
-        check: :test,
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.html": :test
+        check: :test
       ],
       name: "PlugDeviseSession",
       description: "Simple plug for sharing Devise session in Elixir",
@@ -29,7 +25,7 @@ defmodule PlugDeviseSession.Mixfile do
 
   defp package do
     [
-      maintainers: ["Karol Słuszniak"],
+      maintainers: ["Fresha"],
       licenses: ["MIT"],
       links: %{
         "GitHub" => "https://github.com/surgeventures/plug_devise_session"
@@ -50,12 +46,13 @@ defmodule PlugDeviseSession.Mixfile do
   defp deps do
     [
       {:confix, "~> 0.1"},
-      {:credo, "~> 0.9", only: [:dev, :test]},
+      {:credo, "~> 1.0", only: [:dev, :test]},
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:ex_marshal, "~> 0.0.8"},
-      {:excoveralls, "~> 0.7", only: :test},
+      {:junit_formatter, "~> 3.3", only: [:test]},
       {:plug, "~> 1.3.2 or ~> 1.4"},
-      {:plug_rails_cookie_session_store, "~> 1.0.0"}
+      {:plug_rails_cookie_session_store, "~> 2.0"}
     ]
   end
 
